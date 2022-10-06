3e4ab2c58f5dbc28a3d22c58f266ca85ad521abf
SrMazas <info.otero.gabriel@gmail.com>
2022-10-06 13:57:57 +0200

    Save

--- 02-xml-files/Exercise4.java
+++ 02-xml-files/Exercise4.java
@@ -15,7 +15,7 @@ public class Exercise4 {
 
 		try {
 			DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-			dbf.setIgnoringComments(true);
+			// dbf.setIgnoringComments(true);
 
 			DocumentBuilder builder = dbf.newDocumentBuilder();
 			doc = builder.parse(path);
@@ -28,129 +28,37 @@ public class Exercise4 {
 
 	}
 
-	public static void showText(Element root, String tag) {
-		NodeList nodeList = root.getElementsByTagName(tag);
-
+	public static void showNode(Node root, int depth) {
+		NodeList nodeList = root.getChildNodes();
+		int aux;
 		for (int i = 0; i < nodeList.getLength(); i++) {
-			System.out.println(nodeList.item(i).getTextContent());
-		}
-	}
 
-	public static void showNode(Element root, String tag) {
-		NodeList nodeList = root.getElementsByTagName(tag);
-
-		for (int i = 0; i < nodeList.getLength(); i++) {
-			System.out.println(nodeList.item(i).getNodeName());
-		}
-	}
-	
-	public static void showData(Document doc) {
-		NodeList rootList = doc.getElementsByTagName("pelicula");
-		NodeList moviesList;
-		NodeList directorList;
-		NamedNodeMap attributes;
-		Node attribute;
-		Node movie;
-		Node aux;
-		Node auxDirector;
-
-		for (int i = 0; i < rootList.getLength(); i++) {
-			movie = rootList.item(i);
-
-			System.out.println(movie.getTextContent());
-
-			// if (movie.hasAttributes()) {
-			// 	attributes = movie.getAttributes();
-			// 	for (int k = 0; k < attributes.getLength(); k++) {
-			// 		attribute = attributes.item(k);
-
-			// 		if (attribute.getNodeName().equals("genero")) {
-			// 			System.out.println(attribute.getNodeName() + " -> " + attribute.getNodeValue());
-			// 		}
-
-			// 	}
-			// }
-
-			moviesList = movie.getChildNodes();
-			for (int j = 0; j < moviesList.getLength(); j++) {
-				
-				aux = moviesList.item(j);
-
-				System.out.println(aux.getNodeName());
-				
-				if (aux.getNodeType() == Node.ELEMENT_NODE) {
-
-					if (aux.getNodeName().equals("titulo")) {
-						System.out.println(aux.getNodeName() );
-						System.out.println(aux.getFirstChild().getTextContent());
-					}
-
-					if (aux.getNodeName().equals("director")) {
-
-						directorList = aux.getChildNodes();
-
-						for (int k = 0; k < directorList.getLength(); k++) {
-
-							auxDirector = directorList.item(k);
-
-							if (auxDirector.getNodeType() == Node.ELEMENT_NODE) {
-
-								System.out.println(auxDirector.getNodeName() );
-								System.out.println(auxDirector.getNodeValue());
-
-							}
-						}
-					}
-				}
+			for (int j = 0; j < depth; j++) {
+				System.out.print("\t");
+			}
+			System.out.println(nodeList.item(i).getNodeType() + " " + nodeList.item(i).getNodeName());
+			if (nodeList.item(i).getNodeType() == Node.ELEMENT_NODE) {
+				aux = depth;
+				aux++;
+				showNode(nodeList.item(i), aux);
 			}
 		}
-
 	}
 
 	public static void showDataMovies(Document doc) {
-		NodeList rootList = doc.getElementsByTagName("pelicula");
-		NodeList moviesList;
-		NodeList directorList;
-		NamedNodeMap attributes;
-		Node attribute;
-		Node movie;
-		Node aux;
-		Node auxDirector;
-		Node name;
-		Node surname;
-
-		for (int i = 0; i < rootList.getLength(); i++) {
-			movie = rootList.item(i);
-			System.out.println(movie.getParentNode().getNodeName());
-			System.out.println(movie.getTextContent());
-
-			// showText((Element)movie, "fimoteca");
-			// showNode((Element)movie, "pelicula");
-			// showText((Element) movie,"titulo");
-			// showText((Element) movie,"director");
-
-			// System.out.println(movie.getNodeName());
-
-			// if (movie.hasAttributes()) {
-			// 	attributes = movie.getAttributes();
-			// 	for (int k = 0; k < attributes.getLength(); k++) {
-			// 		attribute = attributes.item(k);
-
-			// 		if (attribute.getNodeName().equals("genero")) {
-			// 			System.out.println(attribute.getNodeName() + " -> " + attribute.getNodeValue());
-			// 		}
-
-			// 	}
-			// }
-
-		}
-
+		showNode(doc, 0);
 	}
 
 	public static void main(String[] args) {
+		long startTime;
+		long estimatedTime;
 
-		showData(createTree("peliculas.xml"));
-		// showDataMovies(createTree("peliculas.xml"));
+		startTime = System.currentTimeMillis();
+
+		showDataMovies(createTree("peliculas.xml"));
+
+		estimatedTime = System.currentTimeMillis() - startTime;
+		System.out.println("Time: "+estimatedTime+" ms");
 
 	}
 }
