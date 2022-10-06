bc88da1327eaf8c6b65c255e5484f158336c177d
Gabriel O <info.otero.gabriel@gmail.com>
2022-10-06 18:42:37 +0200

    Save

---
+++ 02-xml-files/Exercise5.java
@@ -0,0 +1,69 @@
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+public class Exercise5 {
+
+	private static Document createTree(String path) {
+		Document doc = null;
+
+		try {
+			DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+			dbf.setIgnoringComments(true);
+
+			DocumentBuilder builder = dbf.newDocumentBuilder();
+			doc = builder.parse(path);
+
+		} catch (Exception e) {
+			System.err.println("Error creating document tree");
+		}
+
+		return doc;
+	}
+
+	public static void showNode(Node root) {
+		NodeList nodeList = root.getChildNodes();
+		for (int i = 0; i < nodeList.getLength(); i++) {
+			System.out.println(nodeList.item(i).getTextContent());
+			if (nodeList.item(i).getNodeType() == Node.ELEMENT_NODE) {
+				showNode(nodeList.item(i));
+			}
+		}
+	}
+
+	private static int findTheNumbersOfElements(NodeList nl, String element) {
+		int cont = 0;
+		Node e;
+
+		for (int i = 0; i < nl.getLength(); i++) {
+			e = nl.item(i);
+			if (e.getNodeType() == Node.ELEMENT_NODE && e.getNodeName().equals(element)) {
+				cont++;
+			}
+		}
+
+		return cont;
+	}
+
+	private static void showMovies(Document doc, int nDirectores) {
+		NodeList nl = doc.getElementsByTagName("pelicula");
+		int numOfMovies = 0;
+
+		for (int i = 0; i < nl.getLength(); i++) {
+			numOfMovies = findTheNumbersOfElements(nl.item(i).getChildNodes(), "director");
+			if (numOfMovies > nDirectores) {
+				showNode(nl.item(i));
+			}
+
+		}
+	}
+
+	public static void main(String[] args) {
+		Document doc = createTree("peliculas.xml");
+		showMovies(doc, 1);
+	}
+
+}
