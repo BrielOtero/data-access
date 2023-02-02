a64b94f45813acfbfddb2992de413b536b07b7e3
SrMazas <info.otero.gabriel@gmail.com>
2023-02-02 13:58:10 +0100

    save

--- 05-web-services/web-service/src/main/java/exercises/Personas.java
+++ 05-web-services/web-service/src/main/java/exercises/Personas.java
@@ -4,14 +4,36 @@ import java.util.ArrayList;
 
 import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 
-@Path("personas")
+@Path("/personas")
 public class Personas {
-	private static ArrayList<Persona >personas= new ArrayList<Persona>();
+	private static ArrayList<Persona> personas = new ArrayList<Persona>();
 
 	@Consumes({ MediaType.APPLICATION_JSON })
-	private void guardar(Persona persona){
-			personas.add(persona);
+	private void guardar(Persona persona) {
+		personas.add(persona);
+	}
+
+	@Produces({MediaType.TEXT_XML})
+	private ArrayList<Persona> listar() {
+		return personas;
+	}
+	
+	@Path("{nombre}")
+	
+	private void ver(@PathParam("nombre") String nombre) {
+
+		personas.forEach(x -> {
+			if (x.getNombre().equals(nombre)) {
+				System.out.println("Id: " + x.getId());
+				System.out.println("Nombre: " + x.getNombre());
+				System.out.println("Sexo: " + x.getSexo());
+				String casado = x.isCasado() ? "Si" : "No";
+				System.out.println("Casado: " + casado);
+			}
+		});
 	}
 }
